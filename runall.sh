e81e6bcb5cde45b32dd6224ca344a4a5c8af94a3
Lin Yen-Hsing <julius52700@gapp.nthu.edu.tw>
2020-06-08 13:08:23 +0800

    Fixed runall prob.

--- runall.sh
+++ runall.sh
@@ -1,8 +1,12 @@
+cp -r Galaxies ./Accretion
 cd Accretion
 ./run.sh
 echo accretion finished
+rm -r Galaxies
 cd ../
+cp -r Galaxies ./Stellar
 cd Stellar
 ./run.sh
 echo stellar finished
+rm -r Galaxies
 cd ../
